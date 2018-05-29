70295f19aad2b7defe1d694f14454de60739e98d
Contra <contra@users.noreply.github.com>
2018-05-29 15:41:12 -0400

    export fix

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,27 @@
-"use strict";
+'use strict';
 
 exports.__esModule = true;
+exports.transform = exports.tap = exports.parse = exports.fetch = undefined;
 
-exports.default = () => {};
+var _fetch = require('./fetch');
 
-module.exports = exports["default"];
+var _fetch2 = _interopRequireDefault(_fetch);
+
+var _parse = require('./parse');
+
+var _parse2 = _interopRequireDefault(_parse);
+
+var _tap = require('./tap');
+
+var _tap2 = _interopRequireDefault(_tap);
+
+var _transform = require('./transform');
+
+var _transform2 = _interopRequireDefault(_transform);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.fetch = _fetch2.default;
+exports.parse = _parse2.default;
+exports.tap = _tap2.default;
+exports.transform = _transform2.default;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-export default () => {}
+import fetch from './fetch'
+import parse from './parse'
+import tap from './tap'
+import transform from './transform'
+
+export { fetch, parse, tap, transform }
