091d29441f86fe55d830b46c3d41e68715412860
contra <yo@contra.io>
2018-06-25 13:34:28 -0400

    better retry logic

--- dist/fetch/fetchURL.js
+++ dist/fetch/fetchURL.js
@@ -10,11 +10,17 @@ var _through = require('through2');
 
 var _through2 = _interopRequireDefault(_through);
 
+var _superagentRetryDelay = require('superagent-retry-delay');
+
+var _superagentRetryDelay2 = _interopRequireDefault(_superagentRetryDelay);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+(0, _superagentRetryDelay2.default)(_superagent2.default); // hook it
 
 exports.default = url => {
   const out = (0, _through2.default)();
-  const req = _superagent2.default.get(url).buffer(false).redirects(10).retry(5).once('response', res => {
+  const req = _superagent2.default.get(url).buffer(false).redirects(10).retry(10, 5000, [401, 404]).once('response', res => {
     if (res.error) out.emit('error', res.error);
   }).once('error', err => out.emit('error', err));
   return req.pipe(out);
--- src/fetch/fetchURL.js
+++ src/fetch/fetchURL.js
@@ -1,12 +1,15 @@
 import request from 'superagent'
 import through2 from 'through2'
+import retry from 'superagent-retry-delay'
+
+retry(request) // hook it
 
 export default (url) => {
   const out = through2()
   const req = request.get(url)
     .buffer(false)
     .redirects(10)
-    .retry(5)
+    .retry(10, 5000, [ 401, 404 ])
     .once('response', (res) => {
       if (res.error) out.emit('error', res.error)
     })
