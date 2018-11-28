53ed187f5b221ad878e578a3b463b1772e3da6fa
Sam McCollum <smccollum@mccollum.enterprises>
2018-11-28 15:45:43 -0800

    get 404 test to pass

--- src/fetch/fetchURL.js
+++ src/fetch/fetchURL.js
@@ -2,12 +2,11 @@ import got from 'got'
 import through2 from 'through2'
 import collect from 'get-stream'
 import pump from 'pump'
-import { getStatusText } from 'http-status-codes'
 import hardClose from '../hardClose'
 
 const sizeLimit = 512000 // 512kb
 const rewriteError = (err) => {
-  if (err.status) return new Error(`Server responded with "${getStatusText(err.status)}"`)
+  if (err.statusCode) return new Error(`Server responded with "${err.statusMessage}"`)
   if (err.code === 'ENOTFOUND') return new Error('Failed to resolve server host')
   if (err.code === 'ECONNRESET') return new Error('Connection to server was lost')
   if (err.timeout) return new Error('Server took too long to respond')
@@ -17,7 +16,7 @@ const httpError = (err, res) => {
   const nerror = rewriteError(err)
   nerror.requestError = true
   nerror.code = res.code
-  nerror.status = res.status
+  nerror.status = res.statusCode
   nerror.headers = res.headers
   nerror.body = res.text
   return nerror
@@ -40,7 +39,6 @@ export default (url, { headers, timeout }={}) => {
   req
     // http errors
     .once('response', async (res) => {
-      console.log('response: ', Object.keys(res))
       if (!res.error) return
       haltEnd = true
       res.text = await collect(errCollector, { maxBuffer: sizeLimit })
