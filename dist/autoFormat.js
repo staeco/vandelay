0580f3c54781012558b683b9f0f4f23583405635
contra <yo@contra.io>
2019-09-12 14:41:46 -0400

    chore: release

--- dist/autoFormat.js
+++ dist/autoFormat.js
@@ -96,19 +96,8 @@ const msftDate = /\/Date\((\d+)(?:([-+])(\d+))?\)\//i;
 const parseMicrosoftDate = v => {
   const res = msftDate.exec(v);
   if (!res) return;
-  const [, rsr, tzop, tzoffset] = res;
-  const ts = parseInt(rsr, 10);
-  let offset = 0;
-
-  if (tzop != null && tzoffset != null) {
-    const east = tzop == '+';
-    const hh = parseInt(tzoffset.slice(0, 2), 10);
-    const mm = parseInt(tzoffset.slice(2), 10);
-    offset = hh * 60 + mm - new Date().getTimezoneOffset();
-    if (east) offset = -offset;
-  }
-
-  return new Date(ts + offset * 60000);
+  const [, rsr,,] = res;
+  return new Date(Number(rsr)); //number is stored as UTC anyway, so ignore the offset when parsing
 }; // order of operations is crucial here
 
 
@@ -148,7 +137,7 @@ const infer = v => {
 
   if (v.length >= minLooseLength) {
     const d = (0, _moment.default)(v, looseDateFormats);
-    if (d.isValid()) return d.toDate();
+    if (d.isValid()) return new Date(Date.parse(v));
   }
 
   return v;
