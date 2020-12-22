3c56c5eb874474c13b251462e8c58626f3a99e30
contra <yo@contra.io>
2020-12-22 00:04:45 -0700

    chore: perf

--- dist/streams/mapStream.js
+++ dist/streams/mapStream.js
@@ -5,7 +5,7 @@ exports.default = void 0;
 
 var _readableStream = require("readable-stream");
 
-var _pipelinePipe = require("pipeline-pipe");
+const defaultHighWaterMark = 16;
 
 const mapStream = (work, options = {}) => {
   if (!work) return new _readableStream.PassThrough(options);
@@ -22,10 +22,64 @@ const mapStream = (work, options = {}) => {
     return stream;
   }
 
-  const stream = new _pipelinePipe.ParallelTransform(work, {
-    objectMode: false,
-    maxParallel: options.concurrency,
+  let pendingFinish;
+  const stream = new _readableStream.Transform({
+    highWaterMark: Math.max(concurrency, defaultHighWaterMark),
     ...options
+  });
+
+  stream._transform = (chunk, _, cb) => {
+    const exec = cb => {
+      ++queueState.inProgress;
+      queueState.maxReached = Math.max(queueState.maxReached, queueState.inProgress);
+      work(chunk, (err, data) => {
+        if (cb) {
+          cb(err, data); // eslint-disable-line
+        } else {
+          if (err) stream.emit('error', err);
+          if (data) stream.push(data);
+        }
+
+        --queueState.inProgress;
+        stream.emit('free');
+      });
+    }; // got space, run it
+
+
+    if (queueState.inProgress < concurrency) {
+      exec();
+      cb();
+      return;
+    } // no space, add to queue
+
+
+    queueState.queue.push(exec.bind(null, cb));
+    queueState.maxQueue = Math.max(queueState.maxQueue, queueState.queue.length);
+  };
+
+  stream._final = cb => {
+    if (isQueueFinished()) {
+      cb();
+      return;
+    }
+
+    pendingFinish = cb;
+  }; // basic queue
+
+
+  const queueState = stream.queueState = {
+    inProgress: 0,
+    maxReached: 0,
+    maxQueue: 0,
+    queue: []
+  };
+
+  const isQueueFinished = () => queueState.inProgress === 0 && queueState.queue.length === 0;
+
+  stream.on('free', () => {
+    const nextWork = queueState.queue.shift();
+    if (nextWork) return nextWork();
+    if (pendingFinish && isQueueFinished()) process.nextTick(pendingFinish);
   });
   return stream;
 };
